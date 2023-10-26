f1bb2abfacc5a2c58fe32ae9b618098c34d06b92
uday <uday1kiran@gmail.com>
2023-10-26 19:32:19 +0530

    allowed hosts

--- iptracker/settings.py
+++ iptracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4n8esu+gkx=-$5uz4&s60#zu45rou_o0#eg+$oajyg57zg^bdr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['udayglobuslive.pythonanywhere.com']
 
 
 # Application definition
